c0ada37f8563374c4d1d7c984dc49eca9341e925
Dmitry <secondapple66346@gmail.com>
2022-02-23 20:10:13 +0300

    add .spp and .h for parent Queue

---
+++ FirstLab/FirstLab/ptQueue.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include"ptQueue.h"
+
+using namespace std;
+
+ptQueue::ptQueue() { last = 0; size = 0; }
+ptQueue::~ptQueue()
+{
+	while (size > 0)
+	{
+		cell* temp = last;
+		last = temp->ptrPrev;
+		size--;
+		delete temp;
+	}
+}
+
+cell* ptQueue::get_last() { return last; }
+
+void ptQueue::push(int elem)
+{
+	cell* newCell = new cell;
+	newCell->ptrPrev = last;
+	last = newCell;
+	last->data = elem;
+	size++;
+}
+
+int ptQueue::pop()
+{
+	cell* temp = last;
+	int tmpData = 0;
+
+	if (temp->ptrPrev == nullptr)
+	{
+		tmpData = temp->data;
+		delete last;
+		last = nullptr;
+		size = 0;
+	}
+	else
+	{
+		while (temp->ptrPrev->ptrPrev != nullptr) { temp = temp->ptrPrev; }
+		tmpData = temp->ptrPrev->data;
+		delete temp->ptrPrev;
+		temp->ptrPrev = nullptr;
+		size--;
+	}
+	return tmpData;
+}
+
+void ptQueue::print()
+{
+	cell* temp = last;
+	while (temp->ptrPrev != nullptr)
+	{
+		cout << temp->data << "-->";
+		temp = temp->ptrPrev;
+	}
+	cout << temp->data << endl;
+}
+
+void ptQueue::merge(ptQueue& Q)
+{
+	int* buffData = new int[Q.size];
+	cell* temp = Q.last;
+	for (int i = Q.size - 1; i >= 0; i--)
+	{
+		buffData[i] = temp->data;
+		temp = temp->ptrPrev;
+	}
+	for (int i = 0; i < Q.size; i++) { this->push(buffData[i]); }
+	delete[] buffData;
+}
+
+void ptQueue::copy(ptQueue& Q)
+{
+	int* buffData = new int[Q.size];
+	cell* temp = Q.last;
+	for (int i = Q.size - 1; i >= 0; i--)
+	{
+		buffData[i] = temp->data;
+		temp = temp->ptrPrev;
+	}
+	for (int i = 0; i < Q.size; i++) { this->push(buffData[i]); }
+	delete[] buffData;
+}
+
+bool ptQueue::blSmthng() { return size == 0 ? true : false; }
---
+++ FirstLab/FirstLab/ptQueue.h
@@ -0,0 +1,24 @@
+#pragma once
+
+typedef struct cell {
+	int data = 0;
+	cell* ptrPrev = nullptr;
+}cell;
+
+class ptQueue {
+private:
+	cell* last = 0;
+protected:
+	int size;
+public:
+	ptQueue();
+	~ptQueue();
+
+	cell* get_last();
+	void push(int elem);
+	int  pop();
+	void print();
+	void merge(ptQueue& Q1);
+	void copy(ptQueue& Q);
+	bool blSmthng();
+};
